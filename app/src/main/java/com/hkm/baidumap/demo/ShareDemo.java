ca7c7092504f0771ecd4cf6a5c51a2d4c9ac84af
世外桃源 <jobhesk@gmail.com>
2015-04-15 18:08:12 +0800

    just tried map on gradle..
    what is issue..

---
+++ app/src/main/java/com/hkm/baidumap/demo/ShareDemo.java
@@ -0,0 +1,25 @@
+package com.hkm.baidumap.demo;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.hkm.baidumap.R;
+
+public class ShareDemo extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_share_demo);
+	}
+
+	public void startShareDemo(View view) {
+		Intent intent = new Intent();
+		intent.setClass(this, ShareDemoActivity.class);
+		startActivity(intent);
+
+	}
+
+}
